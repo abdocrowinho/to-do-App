ceaac6f9813ee42cfdaff453cfc3225bcf3b2fd5
abdocrowinho <ahlyua2@gmail.com>
2024-08-13 16:52:12 +0200

    +feat/add task (design)

---
+++ app/src/main/java/com/example/todo_app/ui/views/fragments/addTask_fragment/addTaskFragment.kt
@@ -0,0 +1,41 @@
+package com.example.todo_app.ui.views.fragments.addTask_fragment
+
+import android.app.Dialog
+import android.content.DialogInterface
+import android.graphics.Color
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.todo_app.R
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class addTaskFragment :BottomSheetDialogFragment() {
+
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+        dialog.setOnShowListener { setupBottomSheet(it) }
+        return dialog
+    }
+
+    private fun setupBottomSheet(dialogInterface: DialogInterface) {
+        val bottomSheetDialog = dialogInterface as BottomSheetDialog
+        val bottomSheet = bottomSheetDialog.findViewById<View>(
+            com.google.android.material.R.id.design_bottom_sheet)
+            ?: return
+        bottomSheet.setBackgroundColor(Color.TRANSPARENT)
+        bottomSheet.setPadding(20,0,20,100)
+
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_add_task,container,false)
+    }
+}
